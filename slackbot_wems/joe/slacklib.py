4fcaa090f199bc1781bedc8a67bd0569ec37d299
Joe <joe@techemstudios.com>
2016-11-10 13:41:41 -0500

    Emoji test and commit from git2go app

--- slackbot_wems/joe/slacklib.py
+++ slackbot_wems/joe/slacklib.py
@@ -1,7 +1,7 @@
 
 import led
 # Put your commands here
-COMMAND1 = "are you awake?"
+COMMAND1 = "are you awake 🌞?"
 COMMAND2 = "what is your full name?"
 COMMAND3 = "what is your favorite snack?"
 COMMAND4 = "green led"
