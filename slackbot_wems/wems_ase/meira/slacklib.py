e0840782b3268d7e0ffbe8fc8ad9d66dfed6495c
jo3t3ch3m <joe@techemstudios.com>
2016-11-10 16:38:17 -0500

    student directory fixes
    changes dash to underscore in directory name. switched to lowercase for
    each student

---
+++ slackbot_wems/wems_ase/meira/slacklib.py
@@ -0,0 +1,15 @@
+# Put your commands here
+COMMAND1 = "~~"
+
+# Your handling code goes in this function
+def handle_command(command):
+    """
+        Determine if the command is valid. If so, take action and return
+        a response, if necessary.
+    """
+    response = ""
+    if command.find(COMMAND1) >= 0:
+        response = "Huh?"
+        
+    return response
+
