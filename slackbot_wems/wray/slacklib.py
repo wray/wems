1ee3400799a4a8a4495732f26d683a1fdc96df3d
Chris <me@chriskefalas.me>
2016-11-10 19:44:46 -0500

    Changed a command

--- slackbot_wems/wray/slacklib.py
+++ slackbot_wems/wray/slacklib.py
@@ -18,7 +18,7 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        response = "I am a simpleton bot."
+        response = "My name is Wemmy! I am a Slack bot that is built by the kids are West End Montessori school! Try asking what I can do."
     elif command.find(COMMAND2) >= 0:
         response = "Not much right now... waiting for students to teach me."
     elif command.find(COMMAND3) >= 0:
