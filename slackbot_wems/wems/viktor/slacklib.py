aad985d74d1ff2a53851a5763e9ae505c776618b
Kent <flappystar05@gmail.com>
2016-11-17 15:35:18 -0500

    Kent!

--- slackbot_wems/wems/viktor/slacklib.py
+++ slackbot_wems/wems/viktor/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
-COMMAND1 = "slime"
+COMMAND1 = "pfs"
+COMMAND2 = "egg"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -8,8 +9,12 @@ def handle_command(command):
         a response, if necessary.
     """
     response = ""
+    
     if command.find(COMMAND1) >= 0:
-        response = "Hi! Today is a SlImEy day!!!"
+        response = "you found an easter egg! PRO_FuZion_SlIME made this easter egg! the next easter egg command is egg"
         
+    elif command.find(COMMAND2) >= 0:
+        responce = "that was the last egg! i might make more!"
+
     return response
 
