3fa2063ec0ed8e4fa4b42e397cf203e89789b844
Meira <meira@theboylehouse.com>
2016-11-17 15:58:03 -0500

    Syntax Meira!

--- slackbot_wems/wems/meira/slacklib.py
+++ slackbot_wems/wems/meira/slacklib.py
@@ -1,5 +1,11 @@
 # Put your commands here
 COMMAND1 = "name 1 spieces of dolphin"
+COMMAND2 = "whats my favorite candy"
+COMMAND3 = "name my favorite breed of dog"
+COMMAND4 = "what do i like" 
+
+
+
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -9,7 +15,14 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        response = "Bottle nose dolphin"
+        response = "Bottlenose dolphin"
+
+    elif command.find(COMMAND2) >= 0:
+        response = "sour patch kids"
+   
+    elif command.find(COMMAND3) >= 0:
+         response = "corgi"
+    elif command.find(COMMAND4) >= 0:
+          response = "doughnut"
         
     return response
-
