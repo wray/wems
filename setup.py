2661640142d6d11578dca429ee0513ecd1391d79
wray <wray@wrayesian.com>
2016-11-18 22:36:08 -0500

    adjusting version to force updates

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ from distutils.core import setup
 
 patch_level = int(time.time())
 
-ver = "0.1." + str(patch_level)[4:]
+ver = "0.1." + str(patch_level)[3:]
 
 setup(
   name = 'slackbot_wems',
   packages = find_packages(),
   version = ver,
   description = 'Python Code for Tech Em Studios WEMS Class',
-  author = 'Tech Em Studios',
+  author = 'Techem Studios',
   author_email = 'wray@techemstudios.com',
   url = 'https://github.com/wray/wems',
   download_url = 'https://github.com/wray/wems/tarball/'+ver,
