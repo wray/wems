d57dfa78327f5368ec5af412da48f22141d3ad0c
joe <joe@techemstudios.com>
2016-11-21 11:15:26 -0500

    Morgan adding dog breed command 2
    Command 2 added b Morgan

--- slackbot_wems/wems/morgan/slacklib.py
+++ slackbot_wems/wems/morgan/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
 COMMAND1 = "~~"
+COMMAND2 = "name 20 breeds of dogs!"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -10,6 +11,8 @@ def handle_command(command):
     response = ""
     if command.find(COMMAND1) >= 0:
         response = "Huh?"
+    elif command.find(COMMAND2) >= 0:
+        response = "black lab, yellow lab, chocolate lab, cute pug, snouser, Great Dane, terrier."
         
     return response
 
