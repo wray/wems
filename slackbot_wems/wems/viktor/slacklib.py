be29205d02d452c0bdb2a86df8d2a58a72986759
joe <joe@techemstudios.com>
2016-11-21 14:43:13 -0500

    Joe updating Viktor's easter egg commands
    "pfs" command and "egg" command

--- slackbot_wems/wems/viktor/slacklib.py
+++ slackbot_wems/wems/viktor/slacklib.py
@@ -1,5 +1,7 @@
 # Put your commands here
 COMMAND1 = "slime"
+COMMAND2 = "pfs"
+COMMAND3 = "egg"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -10,6 +12,11 @@ def handle_command(command):
     response = ""
     if command.find(COMMAND1) >= 0:
         response = "Hi! Today is a SlImEy day!!!"
+    elif command.find(COMMAND2) >=0:
+        response = "you found an easter egg! PRO_FuZion_SlIME made this easter egg! the next easter egg command is egg"
+        
+    elif command.find(COMMAND3) >=0:
+        response = "that was the last egg! i might make more!"
         
     return response
 
