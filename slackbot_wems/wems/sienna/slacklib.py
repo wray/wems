dd1649bc0cf0b09666bb70092b0378cb04d05495
joe <joe@techemstudios.com>
2016-12-12 11:17:07 -0500

    Sienna added command 2

--- slackbot_wems/wems/sienna/slacklib.py
+++ slackbot_wems/wems/sienna/slacklib.py
@@ -1,6 +1,6 @@
 # Put your commands here
 COMMAND1 = "name a venomous snake"
-
+COMMAND1 = "name a school"
 # Your handling code goes in this function
 def handle_command(command):
     """
@@ -10,6 +10,8 @@ def handle_command(command):
     response = ""
     if command.find(COMMAND1) >= 0:
         response = "Indian cobra???"
+    elif command.find(COMMAND1) >= 0:
+        response = "wems"
         
     return response
 
