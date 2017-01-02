dfa705b35997f5dc34f77689ea97e4e4b32fb7f8
Kent <Flappystar05@gmail.com>
2017-01-02 07:08:51 -0500

    aa

--- slackbot_wems/wems/kent/__init__.py
+++ slackbot_wems/wems/kent/__init__.py
@@ -1,6 +1,20 @@
 # Python package
 COMMAND1 = "im a cat"
 
+def good_cat_loop():
+    # create a string to hold the response
+    response = ""
+    for i in range(1000):
+        # Print statements do not get
+        # sent back to slack, they will only
+        # print out where the code is run (on the pi)
+        #print("@wemmy haha")
+
+        # Add to the response:
+        response += "@wemmy good cat "
+
+return response
+
 # Your handling code goes in this function
 def handle_command(command):
     """
@@ -9,8 +23,7 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        for i in range (145):
-            response = "good cat"
+        response += "good cat"
         
     return response
 
