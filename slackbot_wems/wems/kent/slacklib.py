32078fac9df0514bb90442e7e897059540275c3f
Kent <Flappystar05@gmail.com>
2017-01-09 07:48:03 -0500

    made it so im a cat command might work

--- slackbot_wems/wems/kent/slacklib.py
+++ slackbot_wems/wems/kent/slacklib.py
@@ -60,3 +60,31 @@ def handle_command(command):
     elif command.find(COMMAND17) >= 0:
         response = "I know I said you were destined to be a hacker, I meant no hacking me"
     return response
+
+COMMAND1 = "im a cat"
+
+def good_cat_loop():
+    # create a string to hold the response
+    response = ""
+    for i in range(1000):
+        # Print statements do not get
+        # sent back to slack, they will only
+        # print out where the code is run (on the pi)
+        #print("@wemmy haha")
+
+        # Add to the response:
+        response += "@wemmy good cat "
+
+return response
+
+# Your handling code goes in this function
+def handle_command(command):
+    """
+        Determine if the command is valid. If so, take action and return
+        a response, if necessary.
+    """
+    response = ""
+    if command.find(COMMAND1) >= 0:
+        response += good_cat_loop()
+        
+return response
