ef23e0ff5abcfe21566ccb00856e82cabfbd9036
wray <wray@wrayesian.com>
2017-01-21 08:47:26 -0500

    tabs over spaces

--- slackbot_wems/wray/slacklib.py
+++ slackbot_wems/wray/slacklib.py
@@ -13,29 +13,32 @@ COMMAND4 = "name an animal"
 COMMAND6 = "version"
 
 def handle_command(command):
-	"""
+    """
 		Determine if the command is valid. If so, take action and return
 		a response, if necessary.
-	"""
-	response = ""
-	if command.find(COMMAND1) >= 0:
-		response = "My name is Wemmy! I am a Slack bot that is built by the kids are West End Montessori school! Try asking what I can do."
-	elif command.find(COMMAND2) >= 0:
-		response = "Not much right now... waiting for students to teach me."
-	elif command.find(COMMAND3) >= 0:
-		try:
-			temp_c,temp,humidity = temp_humidity.read_temp_humidity()
-			response = "At West End Montessori School, the temperature is " + str(temp) + " and the relative humidity is " + str(humidity)
-		except:
-			response = "At my location, there is a sensor malfunction."
+    """
+    response = ""
+    if command.find(COMMAND1) >= 0:
+        response = "My name is Wemmy! I am a Slack bot that is built by the kids are West End Montessori school! Try asking what I can do."
+    elif command.find(COMMAND2) >= 0:
+        response = "Not much right now... waiting for students to teach me."
+    elif command.find(COMMAND3) >= 0:
+        try:
+            temp_c,temp,humidity = temp_humidity.read_temp_humidity()
+            response = "At West End Montessori School, the temperature is " + str(temp) + " and the relative humidity is " + str(humidity)
+        except:
+            response = "At my location, there is a sensor malfunction."
 
-	elif command.find(COMMAND4) >= 0:
-		http = urllib3.PoolManager()
-		animals = json.loads(http.request('GET','https://www.randomlists.com/data/animals.json').data.decode('utf-8'))['data']
-		response = animals[random.randint(0,len(animals)-1)]
+    elif command.find(COMMAND4) >= 0:
+        http = urllib3.PoolManager()
+        try:
+            animals = json.loads(http.request('GET','https://www.randomlists.com/data/animals.json').data.decode('utf-8'))['data']
+            response = animals[random.randint(0,len(animals)-1)]
+        except:
+            response = "Animals not found"
 
-	elif command.find(COMMAND6) >= 0:
+    elif command.find(COMMAND6) >= 0:
         	response = "$Id$"
 
-	return response
+    return response
 
