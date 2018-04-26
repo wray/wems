a5c0cc3f88ae611630159dc205c13ebd8f12f0dc
joetechem <joe@techemstudios.com>
2018-04-26 16:48:46 -0400

    add student dirs

---
+++ slackbot_wems/wems/spencer/slacklib.py
@@ -0,0 +1,15 @@
+# Put your commands here
+COMMAND1 = "~~"
+
+# Your handling code goes in this function
+def handle_command(command):
+    """
+        Determine if the command is valid. If so, take action and return
+        a response, if necessary.
+    """
+    response = ""
+    if COMMAND1 in command:
+        response = "Huh?"
+        
+    return response
+
