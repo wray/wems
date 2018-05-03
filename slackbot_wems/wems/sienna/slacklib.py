5367429fa9a21a533f55a63f447bf2a578003585
techemstudent <class-info@techemstudios.com>
2018-05-03 16:18:28 -0400

    sienna adding commands

--- slackbot_wems/wems/sienna/slacklib.py
+++ slackbot_wems/wems/sienna/slacklib.py
@@ -1,6 +1,7 @@
 # Put your commands here
-COMMAND1 = "name a venomous snake"
-COMMAND2 = "name a school"
+COMMAND1 = "name a venemos snake"
+COMMAND2 = "name a school"												
+COMMAND3 = "name a harry potter character"  
 # Your handling code goes in this function
 def handle_command(command):
     """
@@ -9,9 +10,10 @@ def handle_command(command):
     """
     response = ""
     if COMMAND1 in command:
-        response = "Indian cobra???"
+        response = "Indian cobra"
     elif COMMAND2 in command:
-        response = "wems"
-        
+		response = "Hogwarts" 
+    elif COMMAND3 in command:
+		response= "Hermione"
     return response
 
