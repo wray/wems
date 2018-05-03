9cd24b796d8a6b82c703abd9d35b6646a3c109de
techemstudent <class-info@techemstudios.com>
2018-05-03 16:28:51 -0400

    talent show!

--- slackbot_wems/wems/naomi/slacklib.py
+++ slackbot_wems/wems/naomi/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
-COMMAND1 = "naomi test"
+COMMAND1 = "who is your sister"
+COMMAND2 = "what are you doing for the talent show"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -8,8 +9,13 @@ def handle_command(command):
         a response, if necessary.
     """
     response = ""
+    # RESPONSE TO COMMAND1
     if COMMAND1 in command:
         response = "naomi test passed!"
+    
+    # RESPONSE TO COMMAND2
+    elif COMMAND2 in command:
+		response = "gymnstics, drawing or magic"
         
     return response
 
