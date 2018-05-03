7806a0d0938d8e77dd7f33296dd312fecfaeccae
info@techemstudios.com <info@techemstudios.com>
2018-05-03 16:41:34 -0400

    spencer's commands

--- slackbot_wems/wems/spencer/slacklib.py
+++ slackbot_wems/wems/spencer/slacklib.py
@@ -1,5 +1,7 @@
 # Put your commands here
 COMMAND1 = "spencer test"
+COMMAND2 = "joi test"
+COMMAND3 = "12*100*160"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -10,6 +12,10 @@ def handle_command(command):
     response = ""
     if COMMAND1 in command:
         response = "spencer's test passed"
+    if COMMAND2 in command:
+        response = "joe test passed"
+    if COMMAND3 in command:
+        response = "192000"
         
     return response
 
