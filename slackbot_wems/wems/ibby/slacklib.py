fb55c3fb4f07c4acb3ff05d4db7d4152bf6f1a8f
techemstudent <class-info@techemstudios>
2018-05-03 16:40:32 -0400

    ibby's commands

--- slackbot_wems/wems/ibby/slacklib.py
+++ slackbot_wems/wems/ibby/slacklib.py
@@ -1,5 +1,6 @@
 # Put your commands here
 COMMAND1 = "ibby test"
+COMMAND2="hi"
 
 # Your handling code goes in this function
 def handle_command(command):
@@ -10,6 +11,7 @@ def handle_command(command):
     response = ""
     if COMMAND1 in command:
         response = "ibby test passed!"
-        
+    if COMMAND2 in command:
+		response = "stinky shoes"
     return response
 
