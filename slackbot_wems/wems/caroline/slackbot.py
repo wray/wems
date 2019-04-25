a1e6371894bb650d3680e960c42fbf4f32d7dccd
EC2 Default User <ec2-user@ip-172-31-43-167.ec2.internal>
2019-04-25 19:52:41 +0000

    My commands

--- slackbot_wems/wems/caroline/slackbot.py
+++ slackbot_wems/wems/caroline/slackbot.py
@@ -1,6 +1,8 @@
 # Put your commands here
-COMMAND1 = ""
-COMMAND2 = ""
+COMMAND1 = "get me a toilet"
+COMMAND2 = "get me a lot of money. "
+COMMAND3 = "black tree"
+COMMAND4 = "a face."
 # Your handling code goes in this function
 def handle_command(command):
     """
@@ -9,10 +11,12 @@ def handle_command(command):
     """
     response = ""
     if command.find(COMMAND1) >= 0:
-        response = ""
+        response = "ok ill get you a toilet"
     elif command.find(COMMAND2) >= 0:
-        response = ""
-
-
+        response = "ok, i'll get you 10000000000000000000066665555577778888888886666666666999999999000000000002222222333333444 dollars"
+    elif command.find(COMMAND3) >= 0:
+        response = "what"
+    elif command.find(COMMAND4) >= 0:
+        response = "i don`t understand."
     return response
 
