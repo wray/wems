b736ebc5f815eb6f1dc24275aa539654422c06a8
chk18 <me@chriskefalas.me>
2019-04-25 14:34:42 -0400

    Small Refresh

---
+++ slackbot_wems/wems/hank/slackbot.py
@@ -0,0 +1,18 @@
+# Put your commands here
+COMMAND1 = ""
+COMMAND2 = ""
+# Your handling code goes in this function
+def handle_command(command):
+    """
+        Determine if the command is valid. If so, take action and return
+        a response, if necessary.
+    """
+    response = ""
+    if command.find(COMMAND1) >= 0:
+        response = ""
+    elif command.find(COMMAND2) >= 0:
+        response = ""
+
+
+    return response
+
